295c20cfca5272d59a194b0ed542b3c8a6208a89
Carlos Peraza <carlosperaza17@gmail.com>
2022-02-11 22:29:49 -0600

    Extract data and generate output file

--- linkedin.py
+++ linkedin.py
@@ -3,6 +3,10 @@ from time import sleep
 from selenium.webdriver.common.keys import Keys
 import parameters
 from parsel import Selector
+import csv
+
+writer = csv.writer(open(parameters.output_file, 'w'))
+writer.writerow(['name', 'job_title', 'location', 'linkedin_url'])
 
 username = parameters.username
 password = parameters.password
@@ -36,13 +40,31 @@ profiles_links = driver.find_elements_by_xpath("//div[@id='search']//a[1]")
 profiles_links = [profile.get_attribute('href') for profile in profiles_links]
 
 for profile in profiles_links:
+    if 'translate.google.com' in profile:
+        continue
     driver.get(profile)
     sleep(5)
 
     sel = Selector(text=driver.page_source)
 
     name = sel.xpath('//h1/text()').extract_first()
-    job_title = sel.xpath('//*[contains(@class,"text-body-medium break-words")]/text()').extract_first().strip()
-    experience = sel.xpath('//section[@id="ember66"]//ul/li/div/div//span[1]/text()').extract()
+    job_title = sel.xpath('//*[contains(@class,"text-body-medium break-words")]/text()').extract_first()
+    if job_title:
+        job_title = job_title.strip()
+    # about = sel.xpath("//section[contains(@class,'artdeco-card ember-view break-words pb3')][1]//span[2]/text()").extract()[1]
+    location = sel.xpath("//span[@class='text-body-small inline t-black--light break-words']//text()").extract_first()
+    if location:
+        location = location.strip()
+    linkedin_url = driver.current_url
+    # experiences = sel.xpath("//section[contains(@class,'artdeco-card ember-view break-words pb3')][4]//div[@class='pvs-list__outer-container']").extract()
+
+    print('\n')
+    print(name)
+    print(job_title)
+    print(location)
+    print(linkedin_url)
+    print('\n')
+
+    writer.writerow([name, job_title, location, linkedin_url])
 
 driver.quit()
--- parameters.py
+++ parameters.py
@@ -1,4 +1,4 @@
-username = ''
-password = ''
+username = 'example_username'
+password = 'mypassword'
 google_search = 'site:linkedin.com/in/ AND "Python developer" AND "Venezuela"'
-output_file='output_results.csv'
+output_file='output_data.csv'
